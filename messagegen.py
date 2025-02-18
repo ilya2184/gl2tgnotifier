44264866fb9db8091484708fb609783f3a5bdf3c
Ilya Kochetkov <ie.kochetkov@yandex.ru>
2025-02-18 13:20:31 +0300

    assignee depricated

--- messagegen.py
+++ messagegen.py
@@ -19,26 +19,30 @@ def prepare_description(description):
 
 def prepare_message(action, data):
     object_attributes = data['object_attributes']
-    assignee = data.get('assignee', {})
+    assignees = data.get('assignees', [])
 
     action_text = ""
     subject = clean_subject(object_attributes['title'])
     description = prepare_description(object_attributes['description'])
-    assignee_name = assignee.get('name', 'Не назначена')
+    
+    # Собираем имена ответственных из массива assignees
+    assignee_names = [assignee['name'] for assignee in assignees if 'name' in assignee]
+    assignee_list = ', '.join(assignee_names) if assignee_names else 'Не назначен'
+    
     separator = "----------"  # Разделитель
 
     if action == "open":
         action_text = f"Открыта задача #{object_attributes['id']}."
-        text = f"{description}\n{separator}\nОтветственный: {assignee_name}"
+        text = f"{description}\n{separator}\nОтветственный: {assignee_list}"
     elif action == "close":
         action_text = f"Закрыта задача #{object_attributes['id']}."
-        text = f"{description}\n{separator}\nОтветственный: {assignee_name}"
+        text = f"{description}\n{separator}\nОтветственный: {assignee_list}"
     elif action == "reopen":
         action_text = f"Повторно открыта задача #{object_attributes['id']}."
-        text = f"{description}\n{separator}\nОтветственный: {assignee_name}"
+        text = f"{description}\n{separator}\nОтветственный: {assignee_list}"
     elif action == "update":
         action_text = f"Обновлена задача #{object_attributes['id']}."
-        text = f"{description}\n{separator}\nОтветственный: {assignee_name}"
+        text = f"{description}\n{separator}\nОтветственный: {assignee_list}"
     else:
         return {"subject": "Ошибка", "text": "Неизвестное действие с задачей"}
 
